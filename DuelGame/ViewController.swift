3d27824573b339191adffe6212821075bda6f8b9
Ilya Biltuev <ilyabiltuev@gmail.com>
2022-03-01 19:25:12 +0300

    Initial Commit

---
+++ DuelGame/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  DuelGame
+//
+//  Created by Илья Билтуев on 01.03.2022.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
