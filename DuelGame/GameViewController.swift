2dd0e44e67aaf417c62cbd7ce1e1dd9ca1f21f09
Ilya Biltuev <ilyabiltuev@gmail.com>
2022-03-03 18:35:26 +0300

    added outlets

--- DuelGame/GameViewController.swift
+++ DuelGame/GameViewController.swift
@@ -9,9 +9,14 @@ import Cocoa
 
 class GameViewController: NSViewController {
 
+    @IBOutlet private weak var playerTurnLabel: NSTextField!
+    
+    @IBOutlet private weak var timeLabel: NSTextField!
+    
+    @IBOutlet private weak var actionResultLabel: NSTextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do view setup here.
     }
-    
 }
